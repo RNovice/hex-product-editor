154d33722d23807dece92856070899ca5c1a58f6
Voke <google@voke.com>
2025-01-23 18:30:39 +0800

    ui: control penal ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import CryptoJS from "crypto-js";
 import Editor from "@monaco-editor/react";
-import maximizeSvg from "./assets/maximize.svg";
-import minimizeSvg from "./assets/minimize.svg";
-import oneColSvg from "./assets/oneCol.svg";
-import threeColSvg from "./assets/threeCol.svg";
 
 import "./App.css";
 import ExpireCountdown from "./components/ExpireCountdown";
+import {
+  maximizeSvg,
+  minimizeSvg,
+  oneColSvg,
+  threeColSvg,
+  saveSvg,
+  trashCanSvg,
+  pasteSvg,
+  plusSvg,
+  reloadSvg,
+} from "./assets/svg";
 
 const { VITE_API_BASE: API_BASE, VITE_SECRET_KEY: SECRET_KEY } = import.meta
   .env;
 const saveLocalWarning = "如果你的帳號密碼與其他平台通用的話不建議使用";
+const filedNotFound = <span className="text-danger">找不到資料</span>;
 
 /*
 https://hexschool.github.io/ec-courses-api-swaggerDoc/
@@ -34,12 +42,13 @@ const App = () => {
   const [isMoreImg3Col, setIsMoreImg3Col] = useState(false);
   const [operationLog, setOperationLog] = useState([]);
   const [logKey, setLogKey] = useState(0);
+  const logRef = useRef();
 
   useEffect(() => {
     async function checkLogin() {
       setIsCheckingLogin(true);
       try {
-        logOperation({msg:'檢查登入狀態'})
+        logOperation({ msg: "檢查登入狀態" });
         const token = document.cookie
           .split("; ")
           .find((row) => row.startsWith("productEditorAuthToken="))
@@ -52,11 +61,11 @@ const App = () => {
         setIsLogin(success);
         if (success) {
           setExpireTime(+localStorage.getItem("authExpire"));
-          logOperation({msg:'已登入'})
+          logOperation({ msg: "登入狀態：已登入" });
         }
       } catch (err) {
         console.error(err);
-        logOperation({msg:'請重新登入', type: 'log-warning'})
+        logOperation({ msg: "請重新登入", type: "log-warning" });
       } finally {
         setIsCheckingLogin(false);
       }
@@ -155,14 +164,20 @@ const App = () => {
     if ([username, password, API_Path].every((e) => !e))
       return localStorage.removeItem("ui");
     const encrypt = (text) => CryptoJS.AES.encrypt(text, SECRET_KEY).toString();
-    localStorage.setItem(
-      "ui",
-      JSON.stringify({
-        pa: API_Path || undefined,
-        nu: username ? encrypt(username) : undefined,
-        wp: password ? encrypt(password) : undefined,
-      })
-    );
+    const userInfoObj = {
+      pa: API_Path || undefined,
+      nu: username ? encrypt(username) : undefined,
+      wp: password ? encrypt(password) : undefined,
+    };
+    logOperation({
+      msg:
+        Object.keys(userInfoObj).length === 0
+          ? "已刪除儲存資料"
+          : "已儲存資料在本地",
+      type: "log-success",
+    });
+    Object.keys(userInfoObj).length === 0;
+    localStorage.setItem("ui", JSON.stringify(userInfoObj));
   }
 
   function handleEditorChange(newValue) {
@@ -182,8 +197,10 @@ const App = () => {
       const allProducts = Object.values(res.data.products);
       setProducts(allProducts);
       setJson(JSON.stringify(allProducts, null, "\t"));
+      logOperation({ msg: `成功撈取資料: ${allProducts.length}筆產品` });
     } catch (err) {
       console.error(err);
+      logOperation({ msg: `撈取資料失敗`, type: "log-err" });
     }
   }
 
@@ -192,7 +209,11 @@ const App = () => {
     const HH = now.getHours().toString().padStart(2, "0");
     const MM = now.getMinutes().toString().padStart(2, "0");
     const SS = now.getSeconds().toString().padStart(2, "0");
-    setOperationLog((pre) => [{ msg: `${HH}:${MM}:${SS} | ${msg}`, type }, ...pre]);
+    logRef.current?.scrollTo(0, 0);
+    setOperationLog((pre) => [
+      { msg: `${HH}:${MM}:${SS} | ${msg}`, type },
+      ...pre,
+    ]);
   }
 
   return (
@@ -207,10 +228,14 @@ const App = () => {
               {products.map((product, i) => (
                 <li
                   key={`aside-product-li-${i}`}
-                  className="list-group-item bg-dark text-light aside-product-li"
+                  className="list-group-item bg-dark text-light aside-product-li d-flex"
                   onClick={() => setViewEditProduct(product)}
                 >
-                  {product?.title || "找不到產品名稱"}
+                  <span>{product?.title || "找不到產品名稱"}</span>
+                  <button className="ms-auto me-2 aside-delete-btn">
+                    
+                  </button>
+                  
                 </li>
               ))}
             </ul>
@@ -333,13 +358,105 @@ const App = () => {
                 }}
               />
             </div>
-            <div className="row bg-dark mx-0" style={{ height: "150px" }}>
-              <div className="col-md-7 p-3">
-                <button className="btn btn-success me-2">Create</button>
-                <button className="btn btn-warning me-2">Edit</button>
-                <button className="btn btn-danger">Delete</button>
+            <div
+              className="row bg-dark mx-0 gap-3 overflow-hidden"
+              style={{ height: "150px" }}
+            >
+              <div className="col-md-5 p-3 d-flex control-penal gap-3">
+                <div className="d-flex flex-column align-items-center">
+                  <div className="d-flex justify-content-center align-items-center control-mode user-select-none">
+                    {editMode ? "編 輯" : " 新 增"}
+                    <br />模 式
+                  </div>
+                  <button
+                    className="control-mode-switch btn btn-success"
+                    onClick={() =>
+                      setEditMode((pre) => {
+                        logOperation({ msg: !pre ? "編輯模式" : "新增模式" });
+                        return !pre;
+                      })
+                    }
+                  >
+                    切換
+                    <span className="control-mode-switch-quote">
+                      {editMode ? "新增" : "編輯"}
+                    </span>
+                    模式
+                  </button>
+                </div>
+                {editMode ? (
+                  <div className="mx-auto d-flex flex-column justify-content-around edit-tools control-tools">
+                    <div className="text-center">
+                      {viewEditProduct.title === undefined ? (
+                        "請選擇要編輯的產品"
+                      ) : (
+                        <ruby>
+                          {viewEditProduct.title}
+                          <rt className="text-secondary">
+                            {viewEditProduct.id}
+                          </rt>
+                        </ruby>
+                      )}
+                    </div>
+                    <div className="">
+                      <button title="保存變更此產品">
+                        <i
+                          style={{ maskImage: `url("${saveSvg}")` }}
+                          className="icon bg-success"
+                        />
+                      </button>
+                      <button title="重製回原本資料">
+                        <i
+                          style={{ maskImage: `url("${reloadSvg}")` }}
+                          className="icon bg-warning"
+                        />
+                      </button>
+                      <button title="刪除此產品">
+                        <i
+                          style={{ maskImage: `url("${trashCanSvg}")` }}
+                          className="icon bg-danger"
+                          onClick={() => {}}
+                        />
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="mx-auto d-flex flex-wrap justify-content-center align-items-center add-new-tools control-tools">
+                    <button title="加新產品">
+                      <i
+                        style={{ maskImage: `url("${plusSvg}")` }}
+                        className="icon bg-warning"
+                        onClick={() => {}}
+                      />
+                    </button>
+                    <button className="text-warning" title="貼上產品">
+                      <i
+                        style={{ maskImage: `url("${pasteSvg}")` }}
+                        className="icon bg-warning"
+                        onClick={() => {}}
+                      />
+                    </button>
+                    <button className="text-danger" title="重新開始">
+                      <i
+                        style={{ maskImage: `url("${reloadSvg}")` }}
+                        className="icon bg-danger"
+                        onClick={() => {}}
+                      />
+                    </button>
+                    <button className="text-success" title="上傳保存">
+                      <i
+                        style={{ maskImage: `url("${saveSvg}")` }}
+                        className="icon bg-success"
+                        onClick={() => {}}
+                      />
+                    </button>
+                  </div>
+                )}
               </div>
-              <div className="col-md-5 bg-log overflow-auto h-100 rounded p-0 position-relative">
+              <div
+                className="col bg-log overflow-auto h-100 rounded p-0 position-relative"
+                ref={logRef}
+              >
                 <h6 className="mb-1 position-sticky sticky-top border-bottom log-title">
                   系統日誌
                 </h6>
@@ -389,116 +506,129 @@ const App = () => {
                   </>
                 )}
               </button>
-              <div className="card-body px-0 d-flex flex-column gap-2">
-                <h5 className="card-title">
-                  <span className="text-secondary">標題：</span>
-                  {viewEditProduct.title}
-                </h5>
-                <div className="d-flex gap-3">
-                  <p className="card-subtitle mb-2">
-                    <span className="text-secondary">分類：</span>
-                    {viewEditProduct.category}
-                  </p>
-                  <p className="card-text">
-                    <span className="text-secondary">是否啟用：</span>
-                    {viewEditProduct.is_enabled !== undefined
-                      ? viewEditProduct.is_enabled
-                        ? "已啟用"
-                        : "未啟用"
-                      : "找不到欄位"}
-                  </p>
-                </div>
-                <div className="d-flex gap-3">
-                  <p className="card-text">
-                    <span className="text-secondary">原價：</span>
-                    {viewEditProduct.origin_price}
-                  </p>
-                  <p className="card-text">
-                    <span className="text-secondary">售價：</span>
-                    {viewEditProduct.price}
-                  </p>
-                  <p className="card-text">
-                    <span className="text-secondary">單位：</span>
-                    {viewEditProduct.unit}
-                  </p>
-                </div>
-                <p className="card-text">
-                  <span className="text-secondary">描述：</span>
-                  <br />
-                  <br />
-                  {viewEditProduct.description}
-                </p>
-                <p className="card-text">
-                  <span className="text-secondary">說明：</span>
-                  <br />
-                  <br />
-                  {viewEditProduct.content}
-                </p>
-              </div>
-              <div className="card-body px-0">
-                <h6>
-                  <span className="text-secondary">更多圖片:</span>
+              {Object.keys(viewEditProduct).length !== 0 && (
+                <>
+                  <div className="card-body px-0 d-flex flex-column gap-2">
+                    <h5 className="card-title">
+                      <span className="text-secondary">標題：</span>
+                      {viewEditProduct.title ?? filedNotFound}
+                    </h5>
+                    <div className="d-flex gap-3">
+                      <p className="card-subtitle mb-2">
+                        <span className="text-secondary">分類：</span>
+                        {viewEditProduct.category ?? filedNotFound}
+                      </p>
+                      <p className="card-text">
+                        <span className="text-secondary">是否啟用：</span>
+                        {viewEditProduct.is_enabled !== undefined
+                          ? viewEditProduct.is_enabled
+                            ? "已啟用"
+                            : "未啟用"
+                          : filedNotFound}
+                      </p>
+                    </div>
+                    <div className="d-flex gap-3">
+                      <p className="card-text">
+                        <span className="text-secondary">原價：</span>
+                        {viewEditProduct.origin_price ?? filedNotFound}
+                      </p>
+                      <p className="card-text">
+                        <span className="text-secondary">售價：</span>
+                        {viewEditProduct.price ?? filedNotFound}
+                      </p>
+                      <p className="card-text">
+                        <span className="text-secondary">單位：</span>
+                        {viewEditProduct.unit ?? filedNotFound}
+                      </p>
+                    </div>
+                    <p className="card-text">
+                      <span className="text-secondary">描述：</span>
+                      <br />
+                      <br />
+                      {viewEditProduct.description ?? filedNotFound}
+                    </p>
+                    <p className="card-text">
+                      <span className="text-secondary">說明：</span>
+                      <br />
+                      <br />
+                      {viewEditProduct.content ?? filedNotFound}
+                    </p>
+                  </div>
+                  <div className="card-body px-0">
+                    <h6>
+                      <span className="text-secondary">更多圖片:</span>
 
-                  <button
-                    className="more-pic-layout-btn"
-                    type="button"
-                    onClick={() => setIsMoreImg3Col(false)}
-                  >
-                    <i
-                      style={{ maskImage: `url("${oneColSvg}")` }}
-                      className="icon"
-                    />
-                  </button>
-                  <button
-                    className="more-pic-layout-btn"
-                    type="button"
-                    onClick={() => setIsMoreImg3Col(true)}
-                  >
-                    <i
-                      style={{ maskImage: `url("${threeColSvg}")` }}
-                      className="icon"
-                    />
-                  </button>
-                </h6>
-                <div className="d-flex flex-wrap">
-                  {viewEditProduct.imagesUrl &&
-                    viewEditProduct.imagesUrl.map((img, i) => (
                       <button
-                        key={`more-image-container-${i}`}
-                        className={`ratio ratio-4x3 mb-3 mx-auto ${
-                          fullWho === `product-more-img-${i}`
-                            ? "full-screen"
-                            : ""
-                        }`}
-                        style={{ width: isMoreImg3Col ? "30%" : "90%" }}
+                        className="more-pic-layout-btn"
+                        type="button"
+                        onClick={() => setIsMoreImg3Col(false)}
                       >
-                        {img && (
-                          <>
-                            <i
-                              style={{
-                                maskImage: `url("${
-                                  fullWho === `product-more-img-${i}`
-                                    ? minimizeSvg
-                                    : maximizeSvg
-                                }")`,
-                              }}
-                              className="full-screen-btn icon"
-                              tabIndex={0}
-                              onClick={(e) =>
-                                handleFullscreen(e, `product-more-img-${i}`)
-                              }
-                            />
-                            <img
-                              src={img}
-                              alt={`${"Deluxe Dog Bed"} - More Image ${i + 1}`}
-                              className="rounded"
-                            />
-                          </>
-                        )}
+                        <i
+                          style={{ maskImage: `url("${oneColSvg}")` }}
+                          className="icon"
+                        />
                       </button>
-                    ))}
-                </div>
-              </div>
+                      <button
+                        className="more-pic-layout-btn"
+                        type="button"
+                        onClick={() => setIsMoreImg3Col(true)}
+                      >
+                        <i
+                          style={{ maskImage: `url("${threeColSvg}")` }}
+                          className="icon"
+                        />
+                      </button>
+                    </h6>
+                    {viewEditProduct.imagesUrl !== undefined ? (
+                      <div className="d-flex flex-wrap">
+                        {viewEditProduct.imagesUrl &&
+                          viewEditProduct.imagesUrl.map((img, i) => (
+                            <button
+                              key={`more-image-container-${i}`}
+                              className={`ratio ratio-4x3 mb-3 mx-auto ${
+                                fullWho === `product-more-img-${i}`
+                                  ? "full-screen"
+                                  : ""
+                              }`}
+                              style={{ width: isMoreImg3Col ? "30%" : "90%" }}
+                            >
+                              {img && (
+                                <>
+                                  <i
+                                    style={{
+                                      maskImage: `url("${
+                                        fullWho === `product-more-img-${i}`
+                                          ? minimizeSvg
+                                          : maximizeSvg
+                                      }")`,
+                                    }}
+                                    className="full-screen-btn icon"
+                                    tabIndex={0}
+                                    onClick={(e) =>
+                                      handleFullscreen(
+                                        e,
+                                        `product-more-img-${i}`
+                                      )
+                                    }
+                                  />
+                                  <img
+                                    src={img}
+                                    alt={`${"Deluxe Dog Bed"} - More Image ${
+                                      i + 1
+                                    }`}
+                                    className="rounded"
+                                  />
+                                </>
+                              )}
+                            </button>
+                          ))}
+                      </div>
+                    ) : (
+                      filedNotFound
+                    )}
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </main>
---
+++ src/assets/svg.js
@@ -0,0 +1,11 @@
+import maximizeSvg from "./maximize.svg";
+import minimizeSvg from "./minimize.svg";
+import oneColSvg from "./oneCol.svg";
+import threeColSvg from "./threeCol.svg";
+import saveSvg from "./save.svg";
+import trashCanSvg from "./trash-can.svg";
+import pasteSvg from "./paste.svg";
+import plusSvg from "./plus.svg";
+import reloadSvg from "./reload.svg";
+
+export { maximizeSvg, minimizeSvg, oneColSvg, threeColSvg, saveSvg, trashCanSvg, pasteSvg, plusSvg, reloadSvg };
